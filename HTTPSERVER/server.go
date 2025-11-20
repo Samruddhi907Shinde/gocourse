8b75c6f872b29b462b03ac4c7409ab2a14510476
Samruddhi Shinde <shindesam907@gmail.com>
2025-11-20 10:12:51 +0530

    Completed till Generics

---
+++ HTTPSERVER/server.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+
+	const severAddr string = "127.0.0.1:3000" // localhost - using port 3000 on this computer
+	// Accepting external requests on port 3000 on our computer
+	// Listen - accepting requests
+	// Serve means sending responses
+	// Server receives requests and sends responses
+
+}
